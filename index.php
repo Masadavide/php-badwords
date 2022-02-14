b54bafbaf5d51f9add634176140a7f22a0294084
Masadavide <davide.masadm@gmail.com>
2022-02-14 17:08:47 +0100

    stringa + lunghezza

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $string = "Così ebbe inizio l'età del Fuoco. Ma presto le fiamme svaniranno, e resterà soltanto l'Oscurità.
+    Perfino ora, restano soltanto le braci e l'uomo non vede la luce, ma solo notti eterne.
+    E tra i viventi si distinguono i portatori del maledetto Segno Oscuro.";
+
+    $len = strlen($string);
+
+?>
+
+
+<h1><?php echo $string?></h1>
+<h2>Lunghezza frase: <?php echo $len?></h2>
